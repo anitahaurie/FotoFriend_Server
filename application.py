5ed0760d2ee93ee778eb7044497e5c53111f61cc
dsecca <davsecc1995@gmail.com>
2017-10-30 23:36:28 -0400

    Final review with Thomas

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 import flask
+import requests
 
 from flask_restful import Resource, Api
 
@@ -9,8 +10,28 @@ class Default(Resource):
     def get(self):
         return "Hello, World!"
 
-api.add_resource(Default, '/')
+class StoreImage(Resource):
+	def post(self):
+		image = flask.request.data
 
+		#Send "upload" variable to S3 bucket
+
+
+		'''
+		if response from S3 bucket is successful
+			response = "Your upload was successful!"
+			response.status = "200 OK" 
+		else
+			response = "Something went wrong. Please try again."
+			response.status = ???
+		'''
+		response = 'Your upload was successful!'
+		return response, 200
+
+api.add_resource(Default, '/')
+api.add_resource(StoreImage, '/storeImage')
+
+#REMINDER: Remove local port 80
 if __name__ == '__main__':
     application.debug = False
     application.run()
